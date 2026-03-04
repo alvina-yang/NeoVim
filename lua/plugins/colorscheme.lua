c9eb883e11f990e39f8ea19b849ac0127209d1ff
Alvina Yang <alvina.yang@shopify.com>
2026-03-04 15:36:47 -0500

    color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,13 +3,17 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    config = function()
-      require("catppuccin").setup({})
-      vim.cmd("colorscheme catppuccin")
-    end,
   },
   { "nvim-tree/nvim-web-devicons", opts = {} },
   { "folke/tokyonight.nvim", lazy = false, priority = 1000 },
-  { "rebelot/kanagawa.nvim", lazy = false, priority = 1000 },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("kanagawa").setup({})
+      vim.cmd("colorscheme kanagawa-wave")
+    end,
+  },
   { "EdenEast/nightfox.nvim", lazy = false, priority = 1000 },
 }
