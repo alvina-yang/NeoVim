eb0ebd6e1ef3873d2dad220773e352c05470f635
Alvina Yang <alvina.yang@shopify.com>
2026-03-05 16:17:37 -0500

    theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -12,7 +12,7 @@ return {
     priority = 1000,
     config = function()
       require("kanagawa").setup({})
-      vim.cmd("colorscheme kanagawa-wave")
+      vim.cmd("colorscheme kanagawa")
     end,
   },
   { "EdenEast/nightfox.nvim", lazy = false, priority = 1000 },
